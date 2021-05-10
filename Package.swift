484b83e6585532277c58f7ac370216efb9494426
moonagic <i@moonagic.com>
2021-05-10 16:31:01 +0800

    Remove platforms.

--- Package.swift
+++ Package.swift
@@ -4,9 +4,6 @@ import PackageDescription
 
 let package = Package(
   name: "Then",
-  platforms: [
-    .macOS(.v10_10), .iOS(.v8), .tvOS(.v9), .watchOS(.v2)
-  ],
   products: [
     .library(name: "Then", targets: ["Then"]),
   ],
