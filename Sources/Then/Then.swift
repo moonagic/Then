01bb6fbb42a745deae34517dbdaf2e0d83b55a15
moonagic <i@moonagic.com>
2021-05-10 16:31:30 +0800

    Make all result discardable.

--- Sources/Then/Then.swift
+++ Sources/Then/Then.swift
@@ -32,18 +32,19 @@ public protocol Then {}
 
 extension Then where Self: Any {
 
-  /// Makes it available to set properties with closures just after initializing and copying the value types.
-  ///
-  ///     let frame = CGRect().with {
-  ///       $0.origin.x = 10
-  ///       $0.size.width = 100
-  ///     }
-  @inlinable
-  public func with(_ block: (inout Self) throws -> Void) rethrows -> Self {
-    var copy = self
-    try block(&copy)
-    return copy
-  }
+    /// Makes it available to set properties with closures just after initializing and copying the value types.
+    ///
+    ///     let frame = CGRect().with {
+    ///       $0.origin.x = 10
+    ///       $0.size.width = 100
+    ///     }
+    @inlinable
+    @discardableResult
+    public func with(_ block: (inout Self) throws -> Void) rethrows -> Self {
+        var copy = self
+        try block(&copy)
+        return copy
+    }
 
   /// Makes it available to execute something with closures.
   ///
@@ -52,37 +53,38 @@ extension Then where Self: Any {
   ///       $0.set("devxoul@gmail.com", forKey: "email")
   ///       $0.synchronize()
   ///     }
-  @inlinable
-  public func `do`(_ block: (Self) throws -> Void) rethrows {
-    try block(self)
-  }
+    @inlinable
+    public func `do`(_ block: (Self) throws -> Void) rethrows {
+        try block(self)
+    }
 
 }
 
 extension Then where Self: AnyObject {
 
-  /// Makes it available to set properties with closures just after initializing.
-  ///
-  ///     let label = UILabel().then {
-  ///       $0.textAlignment = .center
-  ///       $0.textColor = UIColor.black
-  ///       $0.text = "Hello, World!"
-  ///     }
-  @inlinable
-  public func then(_ block: (Self) throws -> Void) rethrows -> Self {
-    try block(self)
-    return self
-  }
+    /// Makes it available to set properties with closures just after initializing.
+    ///
+    ///     let label = UILabel().then {
+    ///       $0.textAlignment = .center
+    ///       $0.textColor = UIColor.black
+    ///       $0.text = "Hello, World!"
+    ///     }
+    @inlinable
+    @discardableResult
+    public func then(_ block: (Self) throws -> Void) rethrows -> Self {
+        try block(self)
+        return self
+    }
 
 }
 
 extension NSObject: Then {}
 
 #if !os(Linux)
-  extension CGPoint: Then {}
-  extension CGRect: Then {}
-  extension CGSize: Then {}
-  extension CGVector: Then {}
+    extension CGPoint: Then {}
+    extension CGRect: Then {}
+    extension CGSize: Then {}
+    extension CGVector: Then {}
 #endif
 
 extension Array: Then {}
@@ -90,7 +92,7 @@ extension Dictionary: Then {}
 extension Set: Then {}
 
 #if os(iOS) || os(tvOS)
-  extension UIEdgeInsets: Then {}
-  extension UIOffset: Then {}
-  extension UIRectEdge: Then {}
+    extension UIEdgeInsets: Then {}
+    extension UIOffset: Then {}
+    extension UIRectEdge: Then {}
 #endif
